b8b1c3ffdf67f7cca93a77b2eab81ab35e1c4c0c
kw7oe <choongkwern@hotmail.com>
2022-02-21 21:36:26 +0800

    Add parse input to command/statement and execute statement

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
-use std::io::Write;
+use std::{io::Write, process::exit};
+
+#[derive(Debug)]
+enum MetaCommand {
+    // Success,
+    Unrecognized,
+}
+
+#[derive(Debug)]
+enum StatementType {
+    Select,
+    Insert,
+}
+
+#[derive(Debug)]
+struct Statement {
+    statement_type: StatementType,
+}
 
 fn main() -> std::io::Result<()> {
     let mut buffer = String::new();
@@ -8,15 +25,55 @@ fn main() -> std::io::Result<()> {
         std::io::stdin().read_line(&mut buffer)?;
 
         let input = buffer.trim();
-        if input.eq(".exit") {
-            return Ok(());
-        } else {
-            println!("Unrecognized command '{input}'.");
+
+        if input.starts_with(".") {
+            match handle_meta_command(&input) {
+                MetaCommand::Unrecognized => println!("Unrecognized command '{input}'."),
+            }
         }
+
+        match prepare_statement(&input) {
+            Ok(statement) => execute_statement(&statement),
+            Err(_reason) => println!("Unrecognized keyword at start of '{input}'."),
+        }
+
+        println!("Executed.");
+        buffer.clear();
     }
 }
 
 fn print_prompt() {
     print!("db > ");
     let _ = std::io::stdout().flush();
+}
+
+fn handle_meta_command(command: &str) -> MetaCommand {
+    if command.eq(".exit") {
+        exit(0)
+    } else {
+        return MetaCommand::Unrecognized;
+    }
+}
+
+fn prepare_statement(input: &str) -> Result<Statement, &str> {
+    if input.starts_with("select") {
+        return Ok(Statement {
+            statement_type: StatementType::Select,
+        });
+    }
+
+    if input.starts_with("insert") {
+        return Ok(Statement {
+            statement_type: StatementType::Insert,
+        });
+    }
+
+    return Err("unrecognized statement");
+}
+
+fn execute_statement(statement: &Statement) {
+    match statement.statement_type {
+        StatementType::Select => println!("do select"),
+        StatementType::Insert => println!("do insert"),
+    }
 }
