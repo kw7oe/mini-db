765a2968cf9126360c737950f99c05a0819651a5
kw7oe <choongkwern@hotmail.com>
2022-04-14 16:24:00 +0800

    [C] Support read file concurrently with test

--- src/storage/disk_manager.rs
+++ src/storage/disk_manager.rs
@@ -4,12 +4,14 @@ use std::{
     io::SeekFrom,
     io::{Read, Seek, Write},
     path::PathBuf,
+    sync::Mutex,
 };
 
 #[derive(Debug)]
 pub struct DiskManager {
-    write_file: File,
-    read_file: File,
+    write_file: Mutex<File>,
+    read_file: Mutex<File>,
+    // read_file: File,
     pub file_len: usize,
 }
 
@@ -27,26 +29,64 @@ impl DiskManager {
         let file_len = read_file.metadata().unwrap().len() as usize;
 
         Self {
-            write_file,
-            read_file,
+            write_file: Mutex::new(write_file),
+            read_file: Mutex::new(read_file),
+            // read_file,
             file_len,
         }
     }
 
-    pub fn write_page(&mut self, page_id: usize, page_bytes: &[u8]) -> Result<(), std::io::Error> {
+    pub fn write_page(&self, page_id: usize, page_bytes: &[u8]) -> Result<(), std::io::Error> {
         let offset = page_id * PAGE_SIZE;
-        self.write_file.seek(SeekFrom::Start(offset as u64))?;
-        self.write_file.write_all(page_bytes)?;
-        self.write_file.flush()
+        let mut write_file = self.write_file.lock().unwrap();
+        write_file.seek(SeekFrom::Start(offset as u64))?;
+        write_file.write_all(page_bytes)?;
+        write_file.flush()
     }
 
-    pub fn read_page(&mut self, page_id: usize) -> Result<[u8; PAGE_SIZE], std::io::Error> {
+    pub fn read_page(&self, page_id: usize) -> Result<[u8; PAGE_SIZE], std::io::Error> {
         let offset = page_id * PAGE_SIZE;
 
         // TODO: probably need to handle when offset < file_len
-        self.read_file.seek(SeekFrom::Start(offset as u64))?;
+        let mut read_file = self.read_file.lock().unwrap();
+        read_file.seek(SeekFrom::Start(offset as u64))?;
         let mut buffer = [0; PAGE_SIZE];
-        self.read_file.read_exact(&mut buffer)?;
+        read_file.read_exact(&mut buffer)?;
         Ok(buffer)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::sync::Arc;
+    use std::thread;
+
+    use super::*;
+
+    #[test]
+    fn read_file_concurrently() {
+        let disk_manager = Arc::new(DiskManager::new("test_file"));
+
+        // Setup file
+        for i in 0..8 {
+            disk_manager.write_page(i, &[i as u8; 4096]).unwrap();
+        }
+
+        // Try 1000 times since concurrency bugs sometime occurs sometimes don't
+        for _ in 0..1000 {
+            let mut handles = vec![];
+            for i in 0..8 {
+                let disk_manager = disk_manager.clone();
+                let handle = thread::spawn(move || disk_manager.read_page(i).unwrap());
+                handles.push((i, handle));
+            }
+
+            for (i, handle) in handles {
+                let result = handle.join().unwrap();
+                assert_eq!(result, [i as u8; 4096]);
+            }
+        }
+
+        let _ = std::fs::remove_file("test_file");
     }
 }
