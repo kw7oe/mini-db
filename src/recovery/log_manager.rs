c8544abb54d270f14d2fd6dd36bf7a8f97817d63
kw7oe <choongkwern@hotmail.com>
2022-08-06 21:26:11 +0800

    swap log buffer with flush buffer once it's full

--- src/recovery/log_manager.rs
+++ src/recovery/log_manager.rs
@@ -1,6 +1,8 @@
+use tracing::trace;
+
 use super::log_record::LogRecord;
 use crate::storage::DiskManager;
-use std::{path::Path, sync::atomic::AtomicU32};
+use std::{path::Path, sync::atomic::AtomicU32, thread::JoinHandle};
 
 const LOG_BUFFER_SIZE: usize = 4096;
 
@@ -10,6 +12,8 @@ struct LogManager {
     persistent_lsn: Option<AtomicU32>,
     log_buffer: [u8; LOG_BUFFER_SIZE],
     flush_buffer: [u8; LOG_BUFFER_SIZE],
+    pub offset: usize,
+    join_handle: Option<JoinHandle<()>>,
 }
 
 impl LogManager {
@@ -20,7 +24,14 @@ impl LogManager {
             persistent_lsn: None,
             log_buffer: [0; LOG_BUFFER_SIZE],
             flush_buffer: [0; LOG_BUFFER_SIZE],
+            offset: 0,
+            join_handle: None,
         }
+    }
+
+    pub fn flush(&self) {
+        trace!("flush WAL to disk");
+        self.disk_manager.write_page(1, &self.flush_buffer).unwrap();
     }
 
     pub fn next_lsn(&self) -> u32 {
@@ -33,7 +44,11 @@ impl LogManager {
             .map(|p_lsn| p_lsn.load(std::sync::atomic::Ordering::SeqCst))
     }
 
-    pub fn append_log(&self, log_record: &mut LogRecord) -> u32 {
+    pub fn log_buffer(&self) -> &[u8] {
+        &self.log_buffer
+    }
+
+    pub fn append_log(&mut self, log_record: &mut LogRecord) -> u32 {
         let lsn = self
             .next_lsn
             .fetch_add(1, std::sync::atomic::Ordering::SeqCst);
@@ -41,6 +56,27 @@ impl LogManager {
         log_record.lsn = Some(lsn);
 
         // TODO: Append to buffer or flush to disk if buffer full?
+        let bytes = bincode::serialize(&log_record).unwrap();
+        println!("bytes: {:?}", bytes);
+        let mut end = self.offset + bytes.len();
+
+        // If our log buffer is full, we swap it with the flush_buffer
+        // so we can just flush to disk from the flush buffer while
+        // continue using the log buffer.
+        if end > self.log_buffer.len() {
+            println!("log buffer full, swapping with flush_buffer");
+            std::mem::swap(&mut self.log_buffer, &mut self.flush_buffer);
+
+            // Reset the range as well.
+            self.offset = 0;
+            end = self.offset + bytes.len();
+
+            // Flush manually once we full.
+            self.flush();
+        }
+
+        self.log_buffer[self.offset..end].copy_from_slice(&bytes[..]);
+        self.offset += bytes.len();
 
         lsn
     }
@@ -53,7 +89,7 @@ mod test {
 
     #[test]
     fn append_log() {
-        let lm = LogManager::new("test.wal");
+        let mut lm = LogManager::new("test.wal");
         let mut lr = LogRecord::new(1, None, LogRecordType::Insert);
         assert_eq!(lm.next_lsn(), 1);
 
@@ -65,5 +101,30 @@ mod test {
         assert_eq!(lr.lsn, Some(2));
 
         _ = std::fs::remove_file("test.wal");
+    }
+
+    #[test]
+    fn swap_and_flush_when_log_buffer_full() {
+        let mut lm = LogManager::new("test.wal");
+        let mut lr = LogRecord::new(1, None, LogRecordType::Insert);
+
+        // Sample LSN to calculate number of lr accurately
+        lr.lsn = Some(1);
+        let bytes = bincode::serialize(&lr).unwrap();
+        let number_of_lr = LOG_BUFFER_SIZE / bytes.len();
+
+        // Reset lsn to None, since it will be added when append_log
+        // is called.
+        lr.lsn = None;
+        for _ in 0..number_of_lr + 1 {
+            lm.append_log(&mut lr);
+        }
+
+        // Offset should be bytes.len(), since we fill the log_buffer
+        // by extra one record. So the new offset should be the same
+        // as the len of a single log record.
+        assert_eq!(lm.offset, bytes.len());
+
+        let _ = std::fs::remove_file("test.wal");
     }
 }
